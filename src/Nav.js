4dcf1133163819b12c3c899103452c090abdd704
hhon <alexhnhon@gmail.com>
2020-05-02 23:50:49 -0700

    create init components

---
+++ src/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Nav() {
+    return (
+        <nav>
+            <h3>logo</h3>
+            <ul>
+                <li>Home</li>
+                <li>Items</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav;
